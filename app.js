4bc3794164d7b65bc2c3b83438e735f8fd953060
AyoubMideche <162918556+AyoubMideche@users.noreply.github.com>
2024-05-13 23:09:12 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+// const FirstName = "Ayoub";
+// const LastName = "Mideche";
+// let Adresse = "Main street";
+// Adresse = "First street";
+// console.log(FirstName);
+
+// const website = "youtube";
+// const url = "https://" + website + ".com";
+// console.log(url);
+
+const Street = "main street";
+const country = "Morrocco";
+let fullmaillingadresse = Street + " " + country;
+console.log(fullmaillingadresse);
